5769f000fe618ca4a97e523ab914679cdde92ec7
nimacalhor <nimakalhor25@gmail.com>
2022-02-24 19:51:04 +0330

    _

--- index.js
+++ index.js
@@ -1,5 +1,1 @@
-// To make regular nouns plural, add ‑s to the end.
-//  If the singular noun ends in ‑s, -ss, -sh, -ch, -x, or -z, add ‑es to the end to make
-
 const checkPlural = str => /([^auios]s$|es$|photos|pianos|halos|volcanos)/gim.test(str)
-console.log(checkPlural("bus"))
